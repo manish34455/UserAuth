60abb93512e5123a07c4506376ce9ca98c965f00
manish34455 <mrkumar33355gmail.com>
2026-03-14 17:56:09 +0530

    update backend URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://userauth-b7q2.onrender.com/api/",
+  baseURL: "https://userauth-backend-210t.onrender.com/api/",
   withCredentials: true, // Send HttpOnly cookies automatically
 });
 
